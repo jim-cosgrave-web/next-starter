7b63a3667967a04ee7f1b060082d5ea704e37513
Jim Cosgrave <jcosgrave@bdo.com>
2020-06-01 20:47:03 -0500

    Added layout components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-// import '../main.css'
+import Layout from '../components/Layout/Layout';
 import './../public/styles/main.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
